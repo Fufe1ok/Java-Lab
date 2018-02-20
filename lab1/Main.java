6d9edf8992e6be12953085ef76780c1758ea854a
Fufe1ok <32967290+Fufe1ok@users.noreply.github.com>
2018-02-20 16:59:30 +0200

    Лаконічніший вигляд даних в консолі

--- lab1/Main.java
+++ lab1/Main.java
@@ -13,18 +13,22 @@ public class Main {
         System.out.println(Parrot.toString());
         System.out.println(Chicken.toString());
 
+        
         SomeBird.printSum();
         Parrot.printSum();
         Chicken.printSum();
         Chicken.printStaticSum();
+        System.out.println(" ");
 
        SomeBird.resetValues("Bird", "wild", "color", 12);
        Parrot.resetValues("Parrot", "Domestic", "Green", 14);
        Chicken.setTotalKind(50);
 
+
         System.out.println(SomeBird.toString());
         System.out.println(Parrot.toString());
         System.out.println(Chicken.toString());
+
 
         Bird.printStaticSum();
         Chicken.printSum();
